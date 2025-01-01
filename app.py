8d766deadbd795c24c9b849c45980d6e4aa46adb
Mohamad Mehdi <mehdi21092005@gmail.com>
2025-01-01 13:18:40 +0200

    message 2

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ app = Flask(__name__,template_folder='client', static_folder='client')
 @app.route('/')
 def index():
   return render_template('index.html')
-  
 
+
+
+
+#hello from VSC
 
 @app.route('/dashboard')
 def second():
