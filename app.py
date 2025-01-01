c1b2e2d2a02ee215340d825bd2d1b194e14a4703
Mohamad Mehdi <mehdi21092005@gmail.com>
2025-01-01 13:10:47 +0200

    renaming and accessing

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ app = Flask(__name__,template_folder='client', static_folder='client')
 
 
 @app.route('/')
-
 def index():
   return render_template('index.html')
+
+@app.route('/dashboard')
+def second():
+  return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
   app.run(debug=True)
