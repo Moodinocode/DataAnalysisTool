0f9b22935252fe6325ff94c9b6e95eac5837c6ae
Mohamad Mehdi <mehdi21092005@gmail.com>
2025-01-02 16:55:15 +0200

    routes skeleton

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+
+
+const router = express.Router();
+
+
+router.post('/signup',)
+router.post('/login')
+router.get('/profile')
+
+export default router
---
+++ server/routes/dataRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+
+
+const router = express.Router();
+
+
+router.post('/analyze',)
+router.get('/prev')
+
+export default router
